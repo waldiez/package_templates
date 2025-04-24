b3ace43dd42d917b339d562d65b4356718d77f39
Lazaros Toumanidis <laztoum@protonmail.com>
2025-04-24 22:33:03 +0300

    update deps, bump to 0.4.4

--- both/my_package/_version.py
+++ both/my_package/_version.py
@@ -9,4 +9,4 @@ Dev:
     See ../scripts/hook.py for more information.
 """
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
--- python_only/my_py_package/_version.py
+++ python_only/my_py_package/_version.py
@@ -6,4 +6,4 @@
 # Handled from the root project or from ../scripts/version.py
 #
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
